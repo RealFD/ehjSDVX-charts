e5bcf446cdfb1071fd49844a33d1e7e56d85ec25
RealFD <92821800+RealFD@users.noreply.github.com>
2025-02-18 20:29:54 +0100

    18.02.2025

--- Collabs/Be Wild/custom/realfd/mods/nerd.lua
+++ Collabs/Be Wild/custom/realfd/mods/nerd.lua
@@ -2,17 +2,17 @@ local s = dofile(background.GetPath().."/realfd/shortcuts.lua")
 
 local t = {}
 
-local cord = mdv.MST_X
+local cord = mdv.MST_Y
 local affection = mdv.MA_ALL-mdv.MA_TRK-mdv.MA_TICK
 
 for i = 1, 4, 1 do
     table.insert(t,"nerd"..i)
 end
 
-s.Create(t[1],cord,mdv.MT_T,s.BTA,affection,5,0)
-s.Create(t[2],cord,mdv.MT_T,s.BTB,affection,5,0)
-s.Create(t[3],cord,mdv.MT_T,s.BTC,affection,5,0)
-s.Create(t[4],cord,mdv.MT_T,s.BTD,affection,5,0)
+s.Create(t[1],cord,mdv.MT_R,s.BTA,affection,5,0)
+s.Create(t[2],cord,mdv.MT_R,s.BTB,affection,5,0)
+s.Create(t[3],cord,mdv.MT_R,s.BTC,affection,5,0)
+s.Create(t[4],cord,mdv.MT_R,s.BTD,affection,5,0)
 
 xero.definemod {"nerd","nerdP","nerdD", function (b,p,d)
     local beat = b
@@ -22,17 +22,11 @@ xero.definemod {"nerd","nerdP","nerdD", function (b,p,d)
     if dex % 2 ~= 0 then
         val = -val
     end
-
-    mod.setEModSplineType(mdv.MST_X)
+    mod.setEModSplineType(mdv.MST_Y)
     mod.setEMod(t[dex])
     for i = 0, 127 do
         local ii = i / 127
-        mod.setModSpline(i,math.sin(val*math.pi*i))
-    end
-    mod.setEModSplineType(mdv.MST_Z)
-    for i = 0, 127 do
-        local ii = i / 127
-        mod.setModSpline(i,math.sin(val*math.pi*i))
+        mod.setModSpline(i,math.sin(math.pi*ii)*val*beat)
     end
 end}
 
